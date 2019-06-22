3468a3d589047b0f5001d2f5fdb3f03e762e64d0
Dmitriy Pak <fly2sky7777@mail.ru>
2019-06-22 18:44:22 +0300

    initial commit

---
+++ src/test/java/ru/projects/edu/spring/task3/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.projects.edu.spring.task3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
